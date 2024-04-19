8f7bc8f3aba1b309782e60216713327511f51dd9
jaqueslucas <lucassouza2347@hotmail.com>
2024-04-19 08:35:26 -0300

    implementacao do algoritimo de sort by na tabela

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,24 +1,30 @@
 import axios from "axios";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { useEffect, useState } from "react";
-import { Box, Skeleton } from "@mui/material";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+} from "@mui/material";
+import { Skeleton } from "@mui/material";
 
-/*
-  1. vamo coloca os botoes de ordenacao
-  2. vamos fazer a ordenação funcionar
-*/
+interface Data {
+  [key: string]: number | string;
+}
+
+type Order = "asc" | "desc";
 
 export default function DataTableTaubate() {
   const [columns, setColumns] = useState<string[]>([]);
-  const [rows, setRows] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [rows, setRows] = useState<Data[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [order, setOrder] = useState<Order>("asc");
+  const [orderBy, setOrderBy] = useState<string>("");
 
   useEffect(() => {
     async function getGrades() {
@@ -28,7 +34,6 @@ export default function DataTableTaubate() {
 
       const sample = response.data[0];
       const keys = Object.keys(sample);
-
       setColumns(keys);
       setRows(response.data);
       setLoading(true);
@@ -36,6 +41,43 @@ export default function DataTableTaubate() {
 
     getGrades();
   }, []);
+
+  const handleRequestSort = (property: string) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const stableSort = (
+    array: Data[],
+    comparator: (a: Data, b: Data) => number,
+  ) => {
+    const stabilizedThis = array.map(
+      (el, index) => [el, index] as [Data, number],
+    );
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      return order;
+    });
+    return stabilizedThis.map((el) => el[0]);
+  };
+
+  const descendingComparator = (a: Data, b: Data) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const sortedRows = stableSort(
+    rows,
+    order === "asc"
+      ? descendingComparator
+      : (a, b) => -descendingComparator(a, b),
+  );
 
   return (
     <Box>
@@ -45,22 +87,26 @@ export default function DataTableTaubate() {
             <TableHead>
               <TableRow>
                 {columns.map((column, columnIndex) => (
-                  <TableCell key={columnIndex}>{column}</TableCell>
+                  <TableCell key={columnIndex}>
+                    <TableSortLabel
+                      active={orderBy === column}
+                      direction={orderBy === column ? order : "asc"}
+                      onClick={() => handleRequestSort(column)}
+                    >
+                      {column}
+                    </TableSortLabel>
+                  </TableCell>
                 ))}
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row, rowIndex) => {
-                return (
-                  <TableRow key={rowIndex}>
-                    {columns.map((column, columnIndex) => {
-                      const value = row[column];
-
-                      return <TableCell key={columnIndex}>{value}</TableCell>;
-                    })}
-                  </TableRow>
-                );
-              })}
+              {sortedRows.map((row, rowIndex) => (
+                <TableRow key={rowIndex}>
+                  {columns.map((column, columnIndex) => (
+                    <TableCell key={columnIndex}>{row[column]}</TableCell>
+                  ))}
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </TableContainer>
