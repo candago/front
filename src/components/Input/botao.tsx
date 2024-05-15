af8eee032e4ed4a4dd98c444a9e9926a596a87f1
RafaelSM21 <rafinhaspfc14@gmail.com>
2024-05-15 15:59:00 -0300

    alterações no funcioameto do login

--- src/components/Input/botao.tsx
+++ src/components/Input/botao.tsx
@@ -1,7 +1,28 @@
-export default function Botao() {
-    return(
-        <>
-        <button id='botao'>Entrar</button>
-        </>
-    )
-}
+import React, { useState } from 'react';
+
+
+const Botao: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleClick = () => {
+    const emailValido = email.includes('@');
+    const senhaValida = senha.length >= 2;
+
+    if (emailValido && senhaValida) {
+      console.log("nice carai");
+    } else {
+      console.log('Email ou senha inválidos');
+    }
+  };
+
+
+  return (
+        <button type='button'
+        id='botao'
+        onClick={handleClick}
+        >Entrar</button>
+  );
+};
+
+export default Botao;
