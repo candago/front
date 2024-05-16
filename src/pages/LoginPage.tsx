0fa9393ae0973087c87d04a5c394f196b46de08e
Rafael <rafael.soares@fatec.sp.gov.br>
2024-05-15 21:31:21 -0300

    - m " fixing merge"

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import Botao from '../components/Input/botao';
-import Senha from '../components/Input/senha';
+import Senha from "../components/Input/senha";
 import Login from '../components/Input/login';
 import { useState } from 'react';
 
